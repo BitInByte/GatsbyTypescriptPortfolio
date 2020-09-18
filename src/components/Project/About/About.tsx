85d1beeb50f54851dc74cd8e8ce58015dd2be466
bitinbyte <bitinbyte@outlook.com>
2020-09-18 19:11:42 +0100

    More visual bugs spoted and fixed

--- src/components/Project/About/About.tsx
+++ src/components/Project/About/About.tsx
@@ -22,22 +22,7 @@ const About: React.FC<Props> = ({ title, html, sourceLink }) => (
     <div
       className={classes.About__content}
       dangerouslySetInnerHTML={{ __html: html }}
-    >
-      {/* Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo quod
-      vero molestiae, iusto odit voluptas placeat, est atque debitis nulla
-      doloribus cum maiores neque! Dolorum illum quibusdam assumenda
-      perspiciatis eligendi. Ullam perspiciatis, aliquam cupiditate dolor cumque
-      quaerat, vitae non reprehenderit facere pariatur delectus laborum. Aliquid
-      doloribus voluptatibus similique amet! Quos rem earum at voluptatibus
-      suscipit vero ad repellendus tempora id! Quaerat modi odit, dolore eos
-      illo enim consequatur accusantium? Tenetur dicta doloremque vitae
-      necessitatibus id perspiciatis ad officiis excepturi soluta fugiat alias
-      laboriosam repudiandae, ex quia asperiores sit rerum voluptates! Natus,
-      praesentium aperiam? Blanditiis eum itaque est maiores modi tenetur
-      accusamus veritatis tempora quam aperiam similique, cupiditate porro,
-      facilis officiis ducimus reprehenderit? Facere non beatae consectetur
-      exercitationem tempora impedit ullam! */}
-    </div>
+    ></div>
     <a
       href={sourceLink}
       aria-label="Source code"
